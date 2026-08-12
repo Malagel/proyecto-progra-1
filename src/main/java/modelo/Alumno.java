6cd28937b0d1935c6227155e3c6cbb38ff3f11a5
alvar <alvar@DESKTOP-IJSQ8LN>
2026-08-11 21:10:59 -0400

    Se creo la clase Alumno

---
+++ src/main/java/modelo/Alumno.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Alumno {
+	private String nombre; 
+	private String rut; 
+	private int edad;
+	
+	private int nivelActual; 
+	
+	
+	//Falta agregar coleccion de ramos aprobados (Avance Curricular) 
+}
