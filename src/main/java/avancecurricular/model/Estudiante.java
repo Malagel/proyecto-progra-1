f5bd8bad3fcd617542aa0e38c36b7767f58d7139
Malagel <cubemasterr@gmail.com>
2026-08-22 18:39:03 -0400

    Vaciar clase estudiante.

--- src/main/java/avancecurricular/model/Estudiante.java
+++ src/main/java/avancecurricular/model/Estudiante.java
@@ -1,11 +1,5 @@
 package avancecurricular.model;
 
 public class Estudiante {
-	private String nombre; 
-	private String rut; 
-	private int edad;
-	
-	private int nivelActual; 
-	
-		//Falta agregar coleccion de ramos aprobados (Avance Curricular) 
+
 }
