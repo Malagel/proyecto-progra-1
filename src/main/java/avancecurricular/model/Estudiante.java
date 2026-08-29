0b44ad5c6c8c0ee05c4da0083080a2927149f076
Malagel <cubemasterr@gmail.com>
2026-08-29 14:25:56 -0400

    Crear clase estudiante parcialmente.

--- src/main/java/avancecurricular/model/Estudiante.java
+++ src/main/java/avancecurricular/model/Estudiante.java
@@ -1,5 +1,19 @@
 package avancecurricular.model;
 
-public class Estudiante {
+import java.util.Objects;
+import java.util.HashSet;
+import java.util.Set;
 
+public class Estudiante extends Persona{
+    private Carrera carrera;
+    private final Set<RegistroAcademico> registrosAcademicos;
+
+    public Estudiante(String rut, String nombre, String idCarrera) {
+        super(rut, nombre);
+
+        this.registrosAcademicos = new HashSet<>();
+        this.idCarrera = Objects.requireNonNull(idCarrera, "El ID de la Carrera no puede ser Nulo.");
+    }
+
+    // faltan getters, setters y metodos relacionados a esta clase
 }
