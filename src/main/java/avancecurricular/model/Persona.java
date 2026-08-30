097a5b972aa4995a4999b5e07c670a199e2b8161
GUSTAVO ᯓ★ <ordenesgustavo13@gmail.com>
2026-08-30 18:25:54 -0400

    se agregó a la clae persona: un contructor vacio y metodos de identidad.

--- src/main/java/avancecurricular/model/Persona.java
+++ src/main/java/avancecurricular/model/Persona.java
@@ -1,14 +1,18 @@
 package avancecurricular.model;
 
 public abstract class Persona {
-	private String rut;
+	private final String rut;
 	private String nombre;
-	
+	// constructor
 	public Persona (String rut, String nombre) {
 		this.rut = rut;
 		this.nombre  = nombre;
 	}
-
+	 public persona (){
+		rut = "No definido";
+		nombre = "No definido";
+	 }
+    // getters y setters
 	public void SetRut (String rut) {
 		this.rut = rut; 
 	}
@@ -23,5 +27,28 @@ public abstract class Persona {
 	public String GetNombre () {
 		return nombre;
 	}
+
+	// metodos de  identidad 
+	
+	@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return Objects.equals(rut, persona.rut);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(rut);
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "rut='" + rut + '\'' +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
 
 } 
